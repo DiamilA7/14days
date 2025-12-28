1f5b73ca58b4e59a5dedd4aadc5595ac69581214
L-ishigami <diamilfall63@gmail.com>
2025-12-28 23:22:44 +0100

    Fix longest common prefix solution

--- 1. Arrays/longestCommonPref.c
+++ 1. Arrays/longestCommonPref.c
@@ -1,16 +1,16 @@
 /*
-Time complexity : O(n)
-Space complexity: O(n)
+Time complexity : O(n*m)
+Space complexity: O(1)
 
 Faced difficulties: I didn't know how cut the string when I find a different letter. 
 Solution: Just give  the current different letter a '\0' 
 
 */
-char *longestCommonPrefix(char **strs, int strsSize)
+char *longestCommonPrefix(char **strs, int strsSize)  
 {
 	for(int c =0;; ++c)// fix a character c
 	{
-	 if(strs[0][c] == '\0')// if all the first string's character are the same than the biginning of the others 
+	 if(strs[0][c] == '\0')// if all the first string's characters are the same than the biginning of the others 
 
         return strs[0];// the longest common prefix is the first string . 			
 		for(int s=1; s<strsSize; ++s)
